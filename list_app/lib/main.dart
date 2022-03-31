3bc564e9d7dcba27e73d42086c0b29b7f1a2ff9c
ignatiusyap <31462590+ignatiusyap@users.noreply.github.com>
2022-03-31 19:00:46 +0800

    Update main.dart

--- list_app/lib/main.dart
+++ list_app/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InputForm(title: 'Flutter Assignment'),
+      home: const InputForm(title: 'Flutter App'),
     );
   }
 }
